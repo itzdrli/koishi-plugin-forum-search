170143708c74cb042d532226a556394af6b2bd7f
Itz_Dr_Li <liletian1231@outlook.com>
2023-07-30 14:50:48 +0000

    hi

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, Logger, Schema, h } from 'koishi'
 
 export const name = 'forum-search'
 
@@ -6,6 +6,11 @@ export interface Config {}
 
 export const Config: Schema<Config> = Schema.object({})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context, config: Config) {
+  ctx.command('search <keyword:text>', { authority: 0 })
+    .action(async ({ session }, keyword) => {
+      keyword = keyword.replace(/ /g, '%20')
+      const url = 'https://forum.koishi.xyz/search?expanded=true&q=' + keyword
+      return url
+    })
 }
